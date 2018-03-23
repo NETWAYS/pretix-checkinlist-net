6b2b7a0100688c3569047a88d61b839aa1c0fc57
Michael Friedrich <michael.friedrich@netways.de>
2018-03-23 16:40:23 +0100

    Update setuptools

--- setup.py
+++ setup.py
@@ -11,19 +11,22 @@ except:
     long_description = open('README.md').read()
 
 setup(
-    name='pretix-checkinlist-net',
-    version='0.0.1',
-    description='Pretix checkin list exporter for NETWAYS',
-    long_description=long_description,
-    url='https://github.com/NETWAYS/pretix-checkinlist-net',
-    author='NETWAYS GmbH',
-    author_email='support@netways.de',
-    license='Apache Software License',
+    name = 'pretix-checkinlist-net',
+    version = '0.0.1',
+    description = 'Pretix checkin list exporter for NETWAYS',
+    long_description = long_description,
+    url = 'https://github.com/NETWAYS/pretix-checkinlist-net',
+    download_url = 'https://github.com/NETWAYS/pretix-invoice-net/archive/v0.0.3.tar.gz',
+    keywords = [ 'pretix', 'tickets', 'events', 'invoice', 'pdf' ],
+    author = 'NETWAYS GmbH',
+    author_email = 'support@netways.de',
+    license = 'Apache Software License',
 
-    install_requires=[],
-    packages=find_packages(exclude=['tests', 'tests.*']),
-    include_package_data=True,
-    entry_points="""
+    # pretix already depends on checkin related packages
+    install_requires = [],
+    packages = find_packages(exclude=['tests', 'tests.*']),
+    include_package_data = True,
+    entry_points = """
 [pretix.plugin]
 pretix_checkinlist_net=pretix_checkinlist_net:PretixPluginMeta
 """,
